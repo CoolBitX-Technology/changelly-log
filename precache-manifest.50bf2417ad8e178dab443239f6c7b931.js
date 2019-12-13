76645e856cef53eb0f5128be20c1c96e88e30816
Anton Cheng <anton@coolbitx.com>
2019-12-13 17:14:45 +0800

    Updates

---
+++ precache-manifest.50bf2417ad8e178dab443239f6c7b931.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23aa6d01cf17afba7fb9d95268c9673a",
+    "url": "/changelly-log/index.html"
+  },
+  {
+    "revision": "35238f8c004f8350ec8b",
+    "url": "/changelly-log/static/css/main.1d59d1eb.chunk.css"
+  },
+  {
+    "revision": "5055c1bb3751934f354f",
+    "url": "/changelly-log/static/js/2.26769d6b.chunk.js"
+  },
+  {
+    "revision": "b1bd7d111be11bf1a60de02093538313",
+    "url": "/changelly-log/static/js/2.26769d6b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35238f8c004f8350ec8b",
+    "url": "/changelly-log/static/js/main.012642b6.chunk.js"
+  },
+  {
+    "revision": "fb3b155d4bf6880c836c",
+    "url": "/changelly-log/static/js/runtime-main.34f5d7e0.js"
+  },
+  {
+    "revision": "1cd0e57536864cd8ad77859333b6f9f2",
+    "url": "/changelly-log/static/media/changelly-logo.1cd0e575.png"
+  }
+]);
